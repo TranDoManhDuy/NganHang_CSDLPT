1e2d09ae9c4101d42983a969caec1d002c51956b
TranManhDuy <trandomanhduy2874@gmail.com>
2025-04-25 19:01:30 +0700

    update

---
+++ nganhang/src/app/login/forgot/ForgotForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ForgotForm() {
+    const [account_number, setAccount_number] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [soDt, setSoDT] = useState("")
+
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Login with", { account_number, soDt, newPassword, confirmPassword });
+    };
+    return (
+        <form onSubmit={handleLogin} className="space-y-5">
+        <div>
+            <label htmlFor="account_number" className="block text-sm font-medium text-gray-700">
+            Số tài khoản
+            </label>
+            <input
+            type="number"
+            id="account_number"
+            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            value={account_number}
+            onChange={(e) => setAccount_number(e.target.value)}
+            required
+            />
+        </div>
+
+        <div>
+            <label htmlFor="soDT" className="block text-sm font-medium text-gray-700">
+            Số điện thoại
+            </label>
+            <input
+            type="number"
+            id="soDT"
+            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            value={soDt}
+            onChange={(e) => setSoDT(e.target.value)}
+            required
+            />
+        </div>
+
+        <div>
+            <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
+            Mật khẩu mới
+            </label>
+            <input
+            type="password"
+            id="newPassword"
+            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+            />
+        </div>
+        
+        <div>
+            <label htmlFor="confirmNewPassword" className="block text-sm font-medium text-gray-700">
+            Xác nhận mật khẩu mới
+            </label>
+            <input
+            type="password"
+            id="confirmNewPassword"
+            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            />
+        </div>
+        
+        <button
+            type="submit"
+            className="w-full rounded-md bg-blue-600 p-2 text-white hover:bg-blue-700 transition duration-200"
+        >
+            Xác nhận
+        </button>
+        </form>
+    );
+}
