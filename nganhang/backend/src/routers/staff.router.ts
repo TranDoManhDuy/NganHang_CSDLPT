e4fd4cca12a4fdcaa8b3174048a2b6a7c9a1a8c5
TranManhDuy <trandomanhduy2874@gmail.com>
2025-06-11 20:35:44 +0700

    TranManhDuy

--- nganhang/backend/src/routers/staff.router.ts
+++ nganhang/backend/src/routers/staff.router.ts
@@ -1,11 +1,12 @@
 import { authenticateToken } from "./../middleware/auth.middleware";
 import { Router } from "express";
 
-import { getAllStaff, getAStaff, postStaff, putStaff } from "../controllers/staff.controller";
+import { chuyennhanvien, getAllStaff, getAStaff, postStaff, putStaff } from "../controllers/staff.controller";
 
 const router_staff = Router();
 router_staff.get("/staff", getAllStaff);
 router_staff.post("/staff", postStaff)
 router_staff.put("/staff", putStaff)
 router_staff.get("/getAStaff", getAStaff)
+router_staff.post("/chuyen_chi_nhanh", chuyennhanvien)
 export {router_staff}
